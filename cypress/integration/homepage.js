7359efccd48d79b6bddcedcc557691cdb4031fc7
Tiago Azevedo <tiago.azevedo@yld.io>
2018-05-10 13:43:59 +0100

    Add some example unit tests

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -1,5 +1,17 @@
+import { baseUrl } from '../support/config';
+
 describe('features/homepage', () => {
   it('visits the homepage', () => {
-    cy.visit('https://localhost:8000/');
+    cy.visit(baseUrl);
+
+    cy.title().should('eq', 'Gatsby Default Starter');
+  });
+
+  it('allows navigation to page 2', () => {
+    cy.visit(baseUrl);
+
+    cy.contains('Go to page 2').click();
+
+    cy.contains('Hi from the second page').should('be.visible');
   });
 });
