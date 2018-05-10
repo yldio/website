95baa6fd9271b7cc41598f86130936e23fdc123c
Tiago Azevedo <tiago.azevedo@yld.io>
2018-05-10 11:03:50 +0100

    Update jest config to ignore public and only run tests from /src and /__tests__

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   setupFiles: ['./spec/enzyme-helper.js'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
+  testMatch: [
+    '<rootDir>/__tests__/**/*.{js,jsx}',
+    '<rootDir>/src/**/*spec.{js,jsx}',
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/public/'],
 };
