b98c2d23da7f513f87b36a3459917a8ec834383d
Tiago Azevedo <tiago.azevedo@yld.io>
2018-05-14 15:36:01 +0100

    Pull in blog data from GraphCMS endpoint defined by environment variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,31 @@
+require('dotenv').config({ path: '.env' });
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-styled-components', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-graphcms',
+      options: {
+        endpoint: process.env.GRAPHCMS_ENDPOINT,
+        token: process.env.GRAPHCMS_TOKEN,
+        query: `{
+          allPosts {
+            id
+            title
+            content
+            authors {
+              name
+              avatar {
+                url
+              }
+            }
+          }
+        }`,
+      },
+    },
+  ],
 };
