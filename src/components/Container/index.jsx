7d07bd0d01b4820a8d9d6e162bad116dc69dd2b3
Tiago Azevedo <tiago.azevedo@yld.io>
2018-05-17 13:42:53 +0100

    Fixed padding on wider container setting

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -4,12 +4,12 @@ import { gridWidth, gridWidthWide, gridColWidth } from '../../styles/spacing';
 
 const maxWidth = {
   normal: `${gridWidth + gridColWidth}px`,
-  wide: `${gridWidthWide + gridColWidth}px`,
+  wide: `${gridWidthWide}px`,
 };
 
 const Container = styled.div`
   margin: 0 auto;
-  padding: 0 ${gridColWidth / 2}px;
+  padding: 0 ${props => (props.wide ? 0 : gridColWidth / 2)}px;
   max-width: ${props => (props.wide ? maxWidth.wide : maxWidth.normal)};
 `;
 
