468a8aa2899aca5b8c0ee2ae860da0c4b2c69ffc
Tiago Azevedo <tiago.azevedo@yld.io>
2018-05-17 15:20:39 +0100

    Add icons and touch up styling on IndustryGrid

--- src/compositions/pages/Home/Industries/IndustryGrid/index.jsx
+++ src/compositions/pages/Home/Industries/IndustryGrid/index.jsx
@@ -1,61 +1,88 @@
 import React from 'react';
-import { Grid, Item, Body, Title, Content } from './styled';
+
+import {
+  faBurn,
+  faCar,
+  faCode,
+  faCreditCard,
+  faCubes,
+  faPlane,
+  faShoppingBag,
+  faTv,
+} from '@fortawesome/free-solid-svg-icons';
+
+import { Grid, Icon, Item, Body, CTABody, Title, Content } from './styled';
 
 const IndustryGrid = () => (
   <Grid>
     <Item>
       <Body>
-        <Title>Technology</Title>
+        <Title>
+          <Icon icon={faCode} /> Technology
+        </Title>
         <Content>Google, Microsoft, Canon</Content>
       </Body>
     </Item>
     <Item>
       <Body>
-        <Title>Technology</Title>
+        <Title>
+          <Icon icon={faCar} /> Automotive
+        </Title>
         <Content>Google, Microsoft, Canon</Content>
       </Body>
     </Item>
     <Item>
       <Body>
-        <Title>Technology</Title>
+        <Title>
+          <Icon icon={faTv} /> Entertainment
+        </Title>
         <Content>Google, Microsoft, Canon</Content>
       </Body>
     </Item>
     <Item>
       <Body>
-        <Title>Technology</Title>
+        <Title>
+          <Icon icon={faCreditCard} /> Financial Services
+        </Title>
         <Content>Google, Microsoft, Canon</Content>
       </Body>
     </Item>
     <Item>
       <Body>
-        <Title>Technology</Title>
+        <Title>
+          <Icon icon={faPlane} /> Travel
+        </Title>
         <Content>Google, Microsoft, Canon</Content>
       </Body>
     </Item>
     <Item>
       <Body>
-        <Title>Technology</Title>
+        <Title>
+          <Icon icon={faBurn} /> Energy
+        </Title>
         <Content>Google, Microsoft, Canon</Content>
       </Body>
     </Item>
     <Item>
       <Body>
-        <Title>Technology</Title>
+        <Title>
+          <Icon icon={faShoppingBag} /> Retail
+        </Title>
         <Content>Google, Microsoft, Canon</Content>
       </Body>
     </Item>
     <Item>
       <Body>
-        <Title>Technology</Title>
+        <Title>
+          <Icon icon={faCubes} /> Blockchain
+        </Title>
         <Content>Google, Microsoft, Canon</Content>
       </Body>
     </Item>
     <Item>
-      <Body>
-        <Title>Technology</Title>
-        <Content>Google, Microsoft, Canon</Content>
-      </Body>
+      <CTABody>
+        <Title>View All Industries →</Title>
+      </CTABody>
     </Item>
   </Grid>
 );
--- src/compositions/pages/Home/Industries/IndustryGrid/styled.js
+++ src/compositions/pages/Home/Industries/IndustryGrid/styled.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
-import { text, textLighter } from '../../../../../styles/colours';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  highlight,
+  text,
+  textLighter,
+  textOnDark,
+} from '../../../../../styles/colours';
 import { spacing } from '../../../../../styles/spacing';
 import breakpoints from '../../../../../styles/breakpoints';
 
 export const Item = styled.li`
-  padding: ${spacing()};
+  padding: ${spacing(1.5)};
   width: 100%;
 
   ${breakpoints.tablet`
@@ -16,18 +22,31 @@ export const Item = styled.li`
   `};
 `;
 
+export const Icon = styled(FontAwesomeIcon)`
+  margin-right: ${spacing(0.5)};
+`;
+
 export const Body = styled.div`
   background: white;
   border: 1px solid #e4e8ef;
   border-radius: 3px;
-  padding: ${spacing(2)};
+  box-shadow: 0 0 10px rgba(161, 160, 197, 0.1);
+  color: ${text};
+  height: 100%;
+  padding: ${spacing(3)};
+`;
+
+export const CTABody = Body.extend`
+  background-color: ${highlight};
+  border: none;
+  box-shadow: none;
+  color: ${textOnDark};
 `;
 
 export const Title = styled.h6`
-  color: ${text};
   font-size: 1.25rem;
   font-weight: 500;
-  margin: 0;
+  margin: 0 0 ${spacing()} 0;
 `;
 
 export const Content = styled.p`
@@ -40,6 +59,6 @@ export const Grid = styled.ul`
   display: flex;
   flex-wrap: wrap;
   list-style-type: none;
-  margin: ${spacing(-1)};
+  margin: ${spacing(8)} ${spacing(-1.5)};
   padding: 0;
 `;
