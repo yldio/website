12d45947c315e0d4e12734f4799e7df0677c745a
Tiago Azevedo <tiago.azevedo@yld.io>
2018-05-17 15:55:18 +0100

    Removed unused styled.js file, added WeEnable section

--- src/compositions/pages/Home/Industries/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { text } from '../../../../styles/colours';
-
-export const Title = styled.h2`
-  color: ${text};
-  font-weight: 500;
-  font-size: 4.5rem;
-`;
---
+++ src/compositions/pages/Home/WeEnable/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import PageSection from '../../../../components/PageSection';
+import PageSectionHeader from '../../../../components/PageSectionHeader';
+
+const WeEnable = () => (
+  <PageSection>
+    <PageSectionHeader title="We enable you to..." />
+  </PageSection>
+);
+
+export default WeEnable;
