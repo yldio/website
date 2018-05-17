f68bfc87b248f228bcf6a563a081048e016a5dac
Tiago Azevedo <tiago.azevedo@yld.io>
2018-05-17 10:47:51 +0100

    Added some missing assertions

--- src/components/Button/spec.jsx
+++ src/components/Button/spec.jsx
@@ -2,8 +2,25 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Button from './index';
 
-test('components/Button snapshot', () => {
-  const wrapper = shallow(<Button />);
+describe('components/Button', () => {
+  const mockComponent = props => <div {...props} />;
 
-  expect(wrapper).toMatchSnapshot();
+  test('snapshot', () => {
+    const wrapper = shallow(<Button />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('custom component snapshot', () => {
+    const wrapper = shallow(<Button component={mockComponent} />);
+    expect(wrapper.text()).toContain('Styled(mockComponent)');
+  });
+
+  test('accepts a button or an anchor tag', () => {
+    const anchor = shallow(<Button component="a" />);
+    expect(anchor.text()).toContain('styled.a');
+
+    const btn = shallow(<Button component="button" />);
+    expect(btn.text()).toContain('styled.button');
+  });
 });
