b5f327fbf5629f2c78d3ab8e87434df72fa63cfa
Tiago Azevedo <tiago.azevedo@yld.io>
2018-05-21 12:01:20 +0100

    Updated Tile from TileGrid to invert control of the styling of its children to the parent component

--- src/compositions/pages/Home/CaseStudies/styled.js
+++ src/compositions/pages/Home/CaseStudies/styled.js
@@ -6,3 +6,23 @@ export const CallToAction = styled.p`
   margin-top: ${spacing(8)};
   text-align: center;
 `;
+
+export const TileContent = styled.div`
+  bottom: 0;
+  left: 0;
+  padding: ${spacing(4)}
+  position: absolute;
+  text-align: bottom;
+`;
+
+export const TileContentTitle = styled.h5`
+  font-weight: 500;
+  font-size: 24px;
+  margin: 0;
+`;
+
+export const TileContentBody = styled.p`
+  font-weight: 300;
+  font-size: 24px;
+  margin: 0;
+`;
