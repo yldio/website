60e9c460cb2581afc3eb41e3a00ba5b8cec29032
Tiago Azevedo <tiago.azevedo@yld.io>
2018-05-22 12:45:34 +0100

    Added a gradient option to PageSection and enabled it in HowWeDoIt and Industries

--- src/components/PageSection/index.js
+++ src/components/PageSection/index.js
@@ -5,6 +5,26 @@ const PageSection = Container.withComponent('section').extend`
   margin-bottom: ${spacing(10)};
   margin-top: ${spacing(10)};
   position: relative;
+
+  ${props =>
+    props.gradient &&
+    `
+    padding-bottom: ${spacing(10)};
+    &:after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: -7.5%;
+      height: 50%;
+      width: 115%;
+      background-image: linear-gradient(
+        -180deg,
+        rgba(255, 255, 255, 0.5) 0%,
+        #f3f5fa 52%
+      );
+      z-index: -1;
+    }
+  `}
 `;
 
 export default PageSection;
