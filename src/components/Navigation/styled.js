86d2b11f29010b9474931bb4ddc54490a0570cff
Tiago Azevedo <tiago.azevedo@yld.io>
2018-05-22 15:09:33 +0100

    Updated Link and Navigation to support active class name styling

--- src/components/Navigation/styled.js
+++ src/components/Navigation/styled.js
@@ -1,8 +1,10 @@
-import GatsbyLink from 'gatsby-link';
+import BaseLink, { activeClassName } from 'components/Link';
 import styled from 'styled-components';
 
 import { highlight, textLight, textOnDark } from 'styles/colours';
 import { spacing } from 'styles/spacing';
+
+const paddedRight = (a, b) => props => (props.padRight ? a : b);
 
 export const List = styled.ul`
   list-style-type: none;
@@ -14,34 +16,40 @@ export const Item = styled.li`
   display: inline-block;
 `;
 
-export const Link = styled(GatsbyLink)`
+export const Link = styled(BaseLink)`
   color: ${textLight};
   display: inline-block;
-  font-size: 18px;
-  padding: ${spacing()} ${spacing(1.5)};
+  font-size: 1.125rem;
+  font-weight: 400;
+  padding-bottom: ${spacing()};
+  padding-left: ${spacing(1.5)};
+  padding-right: ${paddedRight(spacing(3), spacing(1.5))};
+  padding-top: ${spacing()};
   position: relative;
   text-decoration: none;
 
-  &:hover:after {
+  &:after {
     bottom: 0;
-    content: '';
     background-color: ${highlight};
+    content: '';
+    display: none;
     height: 2px;
     left: ${spacing(1.5)};
     position: absolute;
-    width: calc(100% - ${spacing(3)});
+    width: calc(100% - ${paddedRight(spacing(4.5), spacing(3))};);
+  }
+
+  &:hover {
+    text-decoration: none;
+  }
+
+  &:hover:after,
+  &.${activeClassName}:after {
+    display: block;
   }
 `;
 
-export const SecondLastLink = Link.extend`
-  padding-right: ${spacing(3)};
-
-  &:hover:after {
-    width: calc(100% - ${spacing(4.5)});
-  }
-`;
-
-export const LastLink = Link.extend`
+export const ButtonLink = Link.extend`
   background-color: ${highlight};
   border-radius: 1px;
   color: ${textOnDark};
