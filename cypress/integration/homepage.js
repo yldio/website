41cf1145b7c70228d39b9ef839cbdd892337ff38
Tiago Azevedo <tiago.azevedo@yld.io>
2018-05-23 16:18:19 +0100

    Add a few homepage feature tests

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -1,14 +1,22 @@
 describe('features/homepage', () => {
-  it('visits the homepage', () => {
+  it('can land on the homepage', () => {
     cy.visit('/');
-
     cy.title().should('eq', 'YLD | Home');
   });
 
-  it.skip('allows navigation to page 2', () => {
+  it('shows a navigation menu', () => {
     cy.visit('/');
-    cy.contains('Go to page 2').click();
+    cy.getComponent('component:navigation');
+  });
 
-    cy.contains('Hi from the second page').should('be.visible');
+  it('shows relevant homepage sections', () => {
+    cy.visit('/');
+    cy.getComponent('home:build-better');
+    cy.getComponent('home:case-studies');
+    cy.getComponent('home:industries');
+    cy.getComponent('home:we-enable');
+    cy.getComponent('home:how-we-do-it');
+    cy.getComponent('home:community');
+    cy.getComponent('home:services-about-us');
   });
 });
