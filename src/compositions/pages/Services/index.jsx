c8f6ba61591597d5cd5690c2d91a18aaca61f436
Tiago Azevedo <tiago.azevedo@yld.io>
2018-05-23 12:25:59 +0100

    Added ProductList and a PageRule to services

--- src/compositions/pages/Services/index.jsx
+++ src/compositions/pages/Services/index.jsx
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react';
 
 import PageSection from 'components/PageSection';
 import PageHero from 'components/PageHero';
+import PageRule from 'components/PageRule';
+import ProductList from 'compositions/ProductList';
 
 const ServicesPage = () => (
   <Fragment>
@@ -13,6 +15,10 @@ const ServicesPage = () => (
         We enable your organisation to deliver world-class technology and user
         experiences, long after we leave.
       </PageHero>
+    </PageSection>
+    <PageSection>
+      <ProductList />
+      <PageRule />
     </PageSection>
   </Fragment>
 );
