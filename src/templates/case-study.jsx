581d7ca9fba43dbaf42611d64f45db5a73b1c39f
Tiago Azevedo <tiago.azevedo@yld.io>
2018-05-25 11:30:09 +0100

    Use gatsby-image to automatically generate and serve responsive images

--- src/templates/case-study.jsx
+++ src/templates/case-study.jsx
@@ -2,19 +2,15 @@ import React, { Fragment } from 'react';
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import { mapProps } from 'recompose';
-import styled from 'styled-components';
+import GatsbyImage from 'gatsby-image';
 
-import fixedImage from 'fragments/fixed-image';
 import PageHero from 'components/PageHero';
 import PageSection from 'components/PageSection';
 import TagList from 'compositions/templates/case-study/TagList';
 
-const HeroImage = styled.img`
-  width: 100%;
-`;
-
 const CaseStudyTemplate = ({
   heroImage,
+  heroImageAlt,
   shortDescription,
   tags,
   title,
@@ -36,23 +32,21 @@ const CaseStudyTemplate = ({
       </PageHero>
     </PageSection>
     <PageSection wide>
-      <HeroImage src={heroImage} />
+      <GatsbyImage alt={heroImageAlt} {...heroImage.childImageSharp} />
     </PageSection>
   </Fragment>
 );
 
 CaseStudyTemplate.propTypes = {
   heroImage: PropTypes.string.isRequired,
+  heroImageAlt: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   pageTitle: PropTypes.string.isRequired,
   shortDescription: PropTypes.string.isRequired,
   tags: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-export default mapProps(props => ({
-  ...props.data.caseStudiesYaml,
-  heroImage: props.data.caseStudiesYaml.heroImage.childImageSharp.original.src,
-}))(CaseStudyTemplate);
+export default mapProps(props => props.data.caseStudiesYaml)(CaseStudyTemplate);
 
 export const pageQuery = graphql`
   query CaseStudyBySlug($slug: String!) {
@@ -62,8 +56,13 @@ export const pageQuery = graphql`
       tags
       shortDescription
       heroImage {
-        ${fixedImage({ imageWidth: 1230 })}
+        childImageSharp {
+          sizes(quality: 75, maxWidth: 1280) {
+            ...GatsbyImageSharpSizes
+          }
+        }
       }
+      heroImageAlt
     }
   }
 `;
