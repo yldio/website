26e519ad59aa5189dadf19f8344bac0870c7d2c6
Anna Lisowska <anna.lisowska@yld.io>
2018-06-04 17:04:40 +0100

    fixed styling

--- src/compositions/templates/case-study/CustomerQuote/styled.js
+++ src/compositions/templates/case-study/CustomerQuote/styled.js
@@ -1,44 +1,41 @@
 import styled from 'styled-components';
 import { highlight, textLighter, textOnDark } from 'styles/colours';
+import DefaultContainer from 'components/Container';
 
 export const QuoteWrapper = styled.div`
   color: ${textOnDark};
-  margin: -13% 7% 2% 7%;
   background-color: ${highlight};
   position: relative;
   font-weight: 300;
-  line-height: 1.24;
+  line-height: 1.24em;
+  padding: 80px 110px;
 `;
 
-export const Container = styled.div`
-  background-color: transparent;
+export const Container = DefaultContainer.extend`
+  margin-top: -80px;
 `;
-
 export const Quote = styled.p`
+  margin-top: 0;
   font-size: 2.375rem;
-  padding: 77px 111px 20px 107px;
-  line-height: 1.24;
+  line-height: 1.24em;
 `;
 
 export const Author = styled.p`
+  margin-top: 0;
+  margin-bottom: 10px;
   font-size: 1.25rem;
-  padding-left: 107px;
-  margin-bottom: 0px;
   font-weight: 500;
 `;
 
 export const Position = styled.p`
-  margin-top: 0px;
+  margin-top: 0;
   font-size: 1.25rem;
-  padding-left: 107px;
-  padding-bottom: 88px;
+  margin-bottom: 0;
 `;
 
 export const SubDescription = styled.p`
   color: ${textLighter};
   font-size: 1.25rem;
   white-space: pre-line;
-  margin-top: 109px;
-  margin-bottom: 75px;
   font-weight: 300;
 `;
