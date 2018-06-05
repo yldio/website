63d93d04814449f65b68477e486a4881c72d9381
Tiago Azevedo <tiago.azevedo@yld.io>
2018-06-05 13:33:13 +0100

    Fix button styling on BuildBetter section and overall

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -6,8 +6,9 @@ export const withButtonStyles = Component => styled(Component)`
   box-shadow: 0 25px 30px 5px rgba(88, 86, 214, 0.2);
   color: ${textOnDark};
   display: inline-block;
-  font-size: 1.125em;
-  font-weight: 400;
+  font-size: 1.125rem;
+  font-weight: 500;
+  line-height: 1.25rem;
   text-align: center;
   text-decoration: none;
   min-width: 10em;
