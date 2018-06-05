a68a76552e82c8c45db9bc32d4daed81e519e050
Semra Shukrieva <semra.shukrieva@yld.io>
2018-06-05 15:47:45 +0100

    delete key

--- src/compositions/templates/case-study/MetricGrid/index.jsx
+++ src/compositions/templates/case-study/MetricGrid/index.jsx
@@ -7,9 +7,7 @@ import { Grid } from './styled';
 const MetricGrid = ({ content }) => (
   <Grid>
     {content &&
-      content.map((metric, i) => (
-        <Metric key={i} {...metric} max={content.length} />
-      ))}
+      content.map(metric => <Metric {...metric} max={content.length} />)}
   </Grid>
 );
 
