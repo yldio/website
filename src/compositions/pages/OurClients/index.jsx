34d325487ee92b49ac0a1e0412cf585373e01da0
Ania <ania.makes@gmail.com>
2018-06-13 12:30:46 +0100

    our clients - changed navbar and header

---
+++ src/compositions/pages/OurClients/index.jsx
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import PageHero from 'components/PageHero';
+import Container from 'components/Container';
+
+import { Section } from './styled';
+
+const OurClients = () => (
+  <Fragment>
+    <Section data-selector="our-clients:hero">
+      <Container>
+        <PageHero before="Our Clients" white big>
+          We’re proud to work with some amazing clients who are as passionate
+          about their industries as we are about helping them build their
+          technology futures.
+        </PageHero>
+      </Container>
+    </Section>
+  </Fragment>
+);
+
+export default OurClients;
