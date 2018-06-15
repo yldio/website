3f03d7d94f370db931c120d0692f65f17144e9f3
Sérgio Ramos <mail@sergioramos.me>
2018-06-15 15:05:17 +0100

    improve home on mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
+import styled from 'styled-components';
 import { Grid, Row, Col } from 'react-styled-flexboxgrid';
 import { Margin, Padding } from 'styled-components-spacing';
+import remcalc from 'remcalc';
 
 import { H1, H3, H4, Copy } from 'components/typography';
 import Checklist, { Item as CheckItem } from 'components/checklist';
@@ -10,12 +12,23 @@ import Button from 'components/button';
 import Card from 'components/card';
 import Hr from 'components/hr';
 
+import WhatDoWeDo from 'sections/what-we-do';
+import WhatWeEnable from 'sections/what-we-enable';
+import KnownFor from 'sections/what-we-known-for';
 import JoinUs from 'sections/join-us';
 import Header from 'sections/header';
 import Footer from 'sections/footer';
 
 import KnowForBg from 'assets/know-for-bg.png';
 import UnexpectedBg from 'assets/unexpected-bg.png';
+
+const VideoBox = styled.div`
+  width: ${remcalc(465)};
+  height: ${remcalc(288)};
+  background-image: linear-gradient(219deg, #6a7277, #4f565b),
+    linear-gradient(#383e42, #383e42);
+  box-shadow: 10.3px 12.3px 40px 5px rgba(25, 5, 51, 0.15);
+`;
 
 export default () => (
   <Fragment>
@@ -47,146 +60,32 @@ export default () => (
         </Row>
       </Grid>
     </Hero>
+    <WhatDoWeDo />
     <Section>
       <Grid>
-        <Row center="xs" around="xs" top="xs">
-          <Col xs={12} sm={12} md={4}>
-            <Margin top={{ xs: 11, md: -39 }} inline>
-              <Card title="Technology" height="255" width="100%">
-                <Copy>
-                  Driving innovation through digital transformation to build
-                  relentlessly relevant companies
-                </Copy>
-              </Card>
-            </Margin>
+        <Row>
+          <Col xs={12} md={6}>
+            <VideoBox />
           </Col>
-          <Col xs={12} sm={12} md={4}>
-            <Margin top={{ xs: 11, md: -39 }} inline>
-              <Card title="Culture" height="255" width="100%">
-                <Copy>
-                  Creating enduring cultures of engineering & design innovation
-                </Copy>
-              </Card>
-            </Margin>
+          <Col xs={12} md={6}>
+            <VideoBox />
           </Col>
-          <Col xs={12} sm={12} md={4}>
-            <Margin top={{ xs: 11, md: -39 }} inline>
-              <Card title="Training" height="255" width="100%">
-                <Copy>
-                  Originators who train your talent through skills transfer and
-                  pair programming
-                </Copy>
-              </Card>
-            </Margin>
+          <Col xs={12} md={6}>
+            <VideoBox />
+          </Col>
+          <Col xs={12} md={6}>
+            <VideoBox />
           </Col>
         </Row>
-        <Margin top={48} inline>
-          <Row>
-            <Col xs={12} sm={6}>
-              <H3 decorated>What do we do</H3>
-            </Col>
-            <Col xs={12} sm={6}>
-              <Copy>
-                We help companies ignite radical technology innovation and
-                through digital transformation, build the most relentlessly
-                relevant brands.<br />
-                <br />Comfortable with digital change, we know how to create a
-                lasting engineering culture of experimentation, to make sure our
-                clients are too.​<br />
-                <br />We thrive in enabling our clients to unlock the potential
-                of their talent through up-skilling and pair programming,
-                empowering them to stay ahead of the competition and improve
-                their bottom line.
-              </Copy>
-            </Col>
-          </Row>
-        </Margin>
       </Grid>
     </Section>
     <Section>
       <Grid>
-        <Margin top={45} bottom={45}>
-          <Hr />
-        </Margin>
+        <Hr />
       </Grid>
     </Section>
-    <Section>
-      <Grid>
-        <Margin bottom={45}>
-          <Row>
-            <Col xs={12}>
-              <H3 decorated>What we enable you to do</H3>
-            </Col>
-            <Col xs={12}>
-              <Checklist>
-                <Row between="xs">
-                  <Col xs={12} sm={6}>
-                    <Padding right={{ xs: 0, sm: 23 }}>
-                      <CheckItem>
-                        Transform, innovate and differentiate at speed
-                      </CheckItem>
-                      <CheckItem>
-                        Adopt cutting edge technologies - We’re pioneers in JS,
-                        Node.js, React and Design Systems
-                      </CheckItem>
-                      <CheckItem>
-                        Build custom software solutions that become your asset
-                      </CheckItem>
-                    </Padding>
-                  </Col>
-                  <Col xs={12} sm={6}>
-                    <Padding right={{ xs: 0, sm: 23 }}>
-                      <CheckItem>
-                        Grow capability inside your organisation - consulting,
-                        training, pairing and education
-                      </CheckItem>
-                      <CheckItem>Grow a design & engineering culture</CheckItem>
-                      <CheckItem>
-                        Deliver the right user experience at the right time, on
-                        the right channel
-                      </CheckItem>
-                    </Padding>
-                  </Col>
-                </Row>
-              </Checklist>
-            </Col>
-          </Row>
-        </Margin>
-      </Grid>
-    </Section>
-    <Hero bg={KnowForBg} height="467" />
-    <Section dark>
-      <Grid>
-        <Margin top={48} bottom={48} inline>
-          <Row>
-            <Col xs={12} sm={6}>
-              <H3 whiter decorated>
-                What do we do
-              </H3>
-            </Col>
-            <Col xs={12} sm={6}>
-              <Copy white>
-                We help clients rethink their approach for the digital age,
-                together creating a new style of technology leadership.<br />
-                <br />Our team are originators, who have had a profound
-                influence on the early adoption of node.js, Kubernetes and React
-                across the global community and we are proud members of the
-                Node.js Foundation.<br />
-                <br />As a magnet for experimental thinkers from around the
-                world, we like to partner with our clients to rewrite the
-                Silicon Valley playbook on digital transformation.<br />
-                <br />Market challengers, next generation leaders with the
-                finest engineers in Europe.<br />
-                <br />We are Open source in technology and bring a distributed
-                working philosophy. The team passionately encourages a culture
-                of giving back to the community and are always excited to code
-                the future.
-              </Copy>
-            </Col>
-          </Row>
-        </Margin>
-      </Grid>
-    </Section>
+    <WhatWeEnable />
+    <KnownFor />
     <Hero bg={UnexpectedBg} height="513" center>
       <Grid>
         <Row center="xs" middle="xs">
---
+++ src/sections/what-we-known-for.js
@@ -0,0 +1,47 @@
+import React, { Fragment } from 'react';
+import { Grid, Row, Col } from 'react-styled-flexboxgrid';
+import { Margin } from 'styled-components-spacing';
+
+import { H3, Copy } from 'components/typography';
+import Section from 'components/section';
+import Hero from 'components/hero';
+
+import KnowForBg from 'assets/know-for-bg.png';
+
+export default () => (
+  <Fragment>
+    <Hero bg={KnowForBg} height="467" />
+    <Section dark>
+      <Grid>
+        <Margin top={{ xs: 15, sm: 50 }} bottom={{ xs: 15, sm: 50 }} inline>
+          <Row>
+            <Col xs={12} sm={6}>
+              <H3 whiter decorated>
+                What are we known for?
+              </H3>
+            </Col>
+            <Col xs={12} sm={6}>
+              <Copy white>
+                We help clients rethink their approach for the digital age,
+                together creating a new style of technology leadership.<br />
+                <br />Our team are originators, who have had a profound
+                influence on the early adoption of node.js, Kubernetes and React
+                across the global community and we are proud members of the
+                Node.js Foundation.<br />
+                <br />As a magnet for experimental thinkers from around the
+                world, we like to partner with our clients to rewrite the
+                Silicon Valley playbook on digital transformation.<br />
+                <br />Market challengers, next generation leaders with the
+                finest engineers in Europe.<br />
+                <br />We are Open source in technology and bring a distributed
+                working philosophy. The team passionately encourages a culture
+                of giving back to the community and are always excited to code
+                the future.
+              </Copy>
+            </Col>
+          </Row>
+        </Margin>
+      </Grid>
+    </Section>
+  </Fragment>
+);
