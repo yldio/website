681bc7669d31349321bb7828f3659cc83c542137
Sérgio Ramos <mail@sergioramos.me>
2018-06-17 03:39:14 +0100

    add home clients details

--- src/compositions/pages/OurClients/spec.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import OurClients from './index';
-
-test('compositions/pages/OurClients snapshot', () => {
-  const wrapper = shallow(<OurClients />);
-
-  expect(wrapper).toMatchSnapshot();
-});
