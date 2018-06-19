5e884a0c53d009556c22d2c38932a021d52a29d6
Ania <ania.makes@gmail.com>
2018-06-19 16:29:36 +0100

    image on thomas cook is too far left between 1024 and 1280

--- src/sections/case-studies.js
+++ src/sections/case-studies.js
@@ -39,7 +39,7 @@ const studies = [
     objective: 'A Platform update & Improved User Experience',
     bg: TrainlineScreenshots,
     img: (
-      <Col style={{ width: '100%' }} xs={12} lg={6}>
+      <Col xs={12} lg={false}>
         <Margin bottom={{ xs: -6 }}>
           <Row middle="xs" center="xs">
             <Img src={TrainlineScreenshots} />
@@ -217,7 +217,7 @@ const CaseStudy = ({
     >
       <Grid>
         <Row>
-          <Col xs={12} xl={6}>
+          <Col xs={12} lg={6}>
             <Margin top={{ xs: 15, lg: 94 }}>
               <H2 whiter>{name}</H2>
             </Margin>
