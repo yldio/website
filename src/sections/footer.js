9e193eaf9a87196b8b2b672f12e7d2a315074edf
Ania <31079643+AniaMakes@users.noreply.github.com>
2018-06-19 14:33:14 +0100

    Make footer look nicer (#86)
    * fixed a button by changing div width
    
    * changed layout of the footer to show as rows with columns as opposed to columns with rows in mobile view
    
    * finished fiddling with the footer

--- src/sections/footer.js
+++ src/sections/footer.js
@@ -27,44 +27,72 @@ export default () => (
     <Grid>
       <Padding top={{ xs: 15, md: 26 }} bottom={{ xs: 15, md: 36 }}>
         <Row between="xs">
-          <Col xs={6}>
-            <H4 whiter>Contact</H4>
-            <ListLink to="mailto:hello@yld.io">hello@yld.io</ListLink>
-            <ListLink to="tel:+4402035144678">
-              UK: +44 (0) 203 514 4678
-            </ListLink>
-            <ListLink to="tel:+351210000000">
-              Portugal: +351 21 000 0000
-            </ListLink>
+          <Col xs={12} md={6} lg={4}>
+            <Padding bottom={{ xs: 15 }}>
+              <H4 whiter>Contact</H4>
+              <ListLink to="mailto:hello@yld.io">hello@yld.io</ListLink>
+              <ListLink to="tel:+4402035144678">
+                UK: +44 (0) 203 514 4678
+              </ListLink>
+              <ListLink to="tel:+351210000000">
+                Portugal: +351 21 000 0000
+              </ListLink>
+            </Padding>
           </Col>
-          <Col xs={6} md={3}>
-            <H4 whiter>Get Around</H4>
-            <ListLink to="/">Home</ListLink>
-            <ListLink to="/services">Services</ListLink>
-            <ListLink to="/our-clients">Our Clients</ListLink>
-            <ListLink to="/about-us">About Us</ListLink>
-            <ListLink to="/join-us">Join Us</ListLink>
+          <Col xs={12} md={6} lg={4}>
+            <Padding bottom={{ xs: 15 }}>
+              <H4 whiter>Get Around</H4>
+              <Row>
+                <Col xs={6} md={6} lg={12}>
+                  <ListLink to="/">Home</ListLink>
+                  <ListLink to="/services">Services</ListLink>
+
+                  <ListLink to="/our-clients">Our Clients</ListLink>
+                </Col>
+                <Col xs={6} md={6} lg={12}>
+                  <ListLink to="/about-us">About Us</ListLink>
+                  <ListLink to="/join-us">Join Us</ListLink>
+                </Col>
+              </Row>
+            </Padding>
           </Col>
-          <Col xs={6} md={3}>
+
+          <Col xs={12} md={12} lg={4}>
             <H4 whiter>Follow Us</H4>
-            <ListLink to="https://twitter.com/yldio">Twitter</ListLink>
-            <ListLink to="https://github.com/yldio">Github</ListLink>
-            <ListLink to="https://youtube.com">Youtube</ListLink>
+            <Row>
+              <Col xs={4} md={2} lg={12}>
+                <ListLink to="https://twitter.com/yldio">Twitter</ListLink>
+              </Col>
+
+              <Col xs={4} md={2} lg={12}>
+                <ListLink to="https://github.com/yldio">Github</ListLink>
+              </Col>
+
+              <Col xs={4} md={2} lg={12}>
+                <ListLink to="https://youtube.com">Youtube</ListLink>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Padding>
       <Margin bottom={15} inline>
         <Copy white>
-          © 2018 Made with love by YLD. All rights reservered
-          <Link white to="/privacy-policy">
-            Privacy
-          </Link>
-          <Link white to="/terms">
-            Terms
-          </Link>
-          <Link white to="/sitemap">
-            Sitemap
-          </Link>
+          <Row>
+            <Col xs={12}>
+              © 2018 Made with love by YLD. All rights reservered
+            </Col>
+            <Col>
+              <Link white to="/privacy-policy">
+                Privacy
+              </Link>
+              <Link white to="/terms">
+                Terms
+              </Link>
+              <Link white to="/sitemap">
+                Sitemap
+              </Link>
+            </Col>
+          </Row>
         </Copy>
       </Margin>
     </Grid>
