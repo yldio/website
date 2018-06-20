54c460fdbca52f32f8c5b794fde5aa591c3c6f46
Ania <ania.makes@gmail.com>
2018-06-20 17:51:11 +0100

    Mobile header colouring is now as on design

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,15 +3,20 @@ import is from 'styled-is';
 
 import Section from 'components/section';
 import BG from 'assets/page-hero.png';
+import breakpoint from 'styled-components-breakpoint';
 
 export default Section.extend`
   background: url('${props => (props.bg ? props.bg : BG)}');
-  background-position: ${props =>
-    props.position ? props.position : 'right top'};
+  background-position: center bottom;
   background-repeat: no-repeat;
   background-size: ${props => (props.size ? props.size : 'cover')};
   max-width: 100%;
   z-index: 0;
+
+  ${breakpoint('sm')`
+    background-position: ${props =>
+      props.position ? props.position : 'right top'};
+  `};
 
   ${is('height')`
     min-height: ${props => remcalc(props.height)};
@@ -22,4 +27,5 @@ export default Section.extend`
     justify-content: center;
     align-items: center;
   `};
+
 `;
