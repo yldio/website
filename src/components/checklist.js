24eb528ea465e339fc57e4bfac8a49696cd4b1f7
Sérgio Ramos <mail@sergioramos.me>
2018-06-21 10:46:07 +0100

    fix fa import

--- src/components/checklist.js
+++ src/components/checklist.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import remcalc from 'remcalc';
 
