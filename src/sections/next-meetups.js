2b630079c2829325297a2b59916d23c983ef268c
Anna Lisowska <anna.lisowska@yld.io>
2018-06-27 17:34:47 +0100

    improve meetups and next-meetups section

--- src/sections/next-meetups.js
+++ src/sections/next-meetups.js
@@ -1,21 +1,19 @@
 import React, { Fragment } from 'react';
 import { Row, Col } from 'react-styled-flexboxgrid';
-import { Margin } from 'styled-components-spacing';
+import { Margin, Padding } from 'styled-components-spacing';
 import styled from 'styled-components';
 import remcalc from 'remcalc';
-
+import BaseLink from 'gatsby-link';
 import Section from 'components/section';
 import { H5, Copy } from 'components/typography';
 import Flex, { FlexItem } from 'styled-flex-component';
+import { Clock } from 'components/icons';
 
 const DataWrapper = Flex.extend`
   background-color: ${props => props.theme.colors.deeppink};
   color: ${props => props.theme.colors.white};
   text-align: center;
   font-weight: 400;
-  -webkit-box-shadow: 0 10px 6px -6px #777;
-  -moz-box-shadow: 0 10px 6px -6px #777;
-  box-shadow: 0 10px 6px -6px #777;
   height: 100%;
 `;
 
@@ -31,7 +29,7 @@ const Month = styled.div`
   font-size: 18px;
 `;
 
-const Link = styled.div`
+const Link = styled(BaseLink)`
   font-size: ${remcalc(18)};
   line-height: ${remcalc(31)};
   color: ${props => props.theme.colors.deeppink};
@@ -44,6 +42,14 @@ const MeetupsDetails = styled.div`
 const Header = H5.extend`
   padding-top: 22px;
   padding-bottom: 20px;
+`;
+
+const DetailsWrapper = Copy.extend`
+  padding-bottom: 22px;
+`;
+
+const ClockIcon = styled.div`
+  width: 100%;
 `;
 
 export default ({ futureMeetups }) => (
@@ -61,28 +67,31 @@ export default ({ futureMeetups }) => (
           <MeetupsDetails>
             <Margin bottom={{ xs: 15, md: 35 }}>
               <Row>
-                <Col xs={3} md={3} height>
-                  <DataWrapper column alignCenter justifyAround>
+                <Col xs={12} md={3} height>
+                  <DataWrapper column alignCenter justifyCenter>
                     <FlexItem>
                       <Day>{futureMeetup.data}</Day>
-                    </FlexItem>
-                    <FlexItem>
                       <Month>{futureMeetup.month}</Month>
                     </FlexItem>
                   </DataWrapper>
                 </Col>
-                <Col xs={5} md={5}>
-                  <Copy>
-                    <Header darker>{futureMeetup.title}</Header>
-                    <p>{futureMeetup.description}</p>
-                    <Link>{futureMeetup.linkB}</Link>
-                  </Copy>
+                <Col xs={12} md={5}>
+                  <Padding left={{ xs: 20 }} right={{ xs: 20 }}>
+                    <DetailsWrapper>
+                      <Header darker>{futureMeetup.title}</Header>
+                      <p>{futureMeetup.description}</p>
+                      <Link>{futureMeetup.SignUpLink}</Link>
+                    </DetailsWrapper>
+                  </Padding>
                 </Col>
-                <Col xs={4} md={4}>
-                  <Copy>
-                    <Header darker>{futureMeetup.hour}</Header>
-                    <p>{futureMeetup.address}</p>
-                  </Copy>
+                <Col xs={12} md={4}>
+                  <Padding left={{ xs: 20 }} right={{ xs: 20 }}>
+                    <DetailsWrapper>
+                      <ClockIcon icon={<Clock />} />
+                      <Header darker>{futureMeetup.hour}</Header>
+                      <p>{futureMeetup.address}</p>
+                    </DetailsWrapper>
+                  </Padding>
                 </Col>
               </Row>
             </Margin>
