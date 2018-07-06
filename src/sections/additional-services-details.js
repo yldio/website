753da16522687e7ebb10b53ac76f7f764c0af3bc
Anna Lisowska <anna.lisowska@yld.io>
2018-07-06 11:08:15 +0100

    changed layout

--- src/sections/additional-services-details.js
+++ src/sections/additional-services-details.js
@@ -1,21 +1,26 @@
 import React, { Fragment } from 'react';
 import { Grid, Row, Col } from 'react-styled-flexboxgrid';
-import { Margin } from 'styled-components-spacing';
+import { Margin, Padding } from 'styled-components-spacing';
+import remcalc from 'remcalc';
 
 import { H2, H4, H5, Copy } from 'components/typography';
 import Hero from 'components/hero';
 import Section from 'components/section';
 import Header from 'sections/header';
 
+const Title = H5.extend`
+  margin-bottom: ${remcalc(20)};
+`;
+
 export default ({ additionalService }) => {
   return (
     <Fragment>
-      <Section>
-        <Hero height="400">
-          <Margin bottom={28}>
-            <Header dark />
-          </Margin>
-          <Grid>
+      <Hero height="400">
+        <Margin bottom={28}>
+          <Header dark />
+        </Margin>
+        <Grid>
+          <Row>
             <Col xs={12} md={10}>
               <Margin bottom={9}>
                 <H2 whiter>{additionalService.headerTitle}</H2>
@@ -26,40 +31,42 @@ export default ({ additionalService }) => {
                 <Copy whiter>{additionalService.headerSubtitle}</Copy>
               </Margin>
             </Col>
-          </Grid>
-        </Hero>
-      </Section>
-      <Section>
-        <Grid>
-          <Row>
-            <Col xs={12} md={10}>
-              <Margin bottom={{ xs: 9, md: 24 }}>
-                <H5>{additionalService.servicesTitle}</H5>
-                <H4>
-                  {additionalService.servicesDescription.servicesDescription}
-                </H4>
-              </Margin>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} md={6}>
-              <Margin bottom={{ xs: 9, md: 24 }}>
-                <H5>{additionalService.subtitleLeft}</H5>
-                <Copy>
-                  {additionalService.subdescriptionLeft.subdescriptionLeft}
-                </Copy>
-              </Margin>
-            </Col>
-            <Col xs={12} md={6}>
-              <Margin bottom={{ xs: 9, md: 24 }}>
-                <H5>{additionalService.subtitleRight}</H5>
-                <Copy>
-                  {additionalService.subdescriptionRight.subdescriptionRight}
-                </Copy>
-              </Margin>
-            </Col>
           </Row>
         </Grid>
+      </Hero>
+      <Section>
+        <Padding top={{ xs: 15, md: 36 }} bottom={{ xs: 15, md: 36 }}>
+          <Grid>
+            <Row>
+              <Col xs={12} md={10}>
+                <Margin bottom={{ xs: 9, md: 24 }}>
+                  <Title>{additionalService.servicesTitle}</Title>
+                  <H4>
+                    {additionalService.servicesDescription.servicesDescription}
+                  </H4>
+                </Margin>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} md={6}>
+                <Margin bottom={{ xs: 9, md: 24 }}>
+                  <Title>{additionalService.subtitleLeft}</Title>
+                  <Copy>
+                    {additionalService.subdescriptionLeft.subdescriptionLeft}
+                  </Copy>
+                </Margin>
+              </Col>
+              <Col xs={12} md={6}>
+                <Margin bottom={{ xs: 9, md: 24 }}>
+                  <Title>{additionalService.subtitleRight}</Title>
+                  <Copy>
+                    {additionalService.subdescriptionRight.subdescriptionRight}
+                  </Copy>
+                </Margin>
+              </Col>
+            </Row>
+          </Grid>
+        </Padding>
       </Section>
     </Fragment>
   );
