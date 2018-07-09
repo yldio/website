9b90f7fabc442ed811645f96383e8de1698fd09c
Anna Lisowska <anna.lisowska@yld.io>
2018-07-09 17:28:03 +0100

    changed png to svg and grid system

--- src/sections/stats.js
+++ src/sections/stats.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-styled-flexboxgrid';
-import { Margin, Padding } from 'styled-components-spacing';
+import { Padding } from 'styled-components-spacing';
+import Flex, { FlexItem } from 'styled-flex-component';
 
 import { Copy, H5 } from 'components/typography';
 import Section from 'components/section';
-import styled from 'styled-components';
-import RevenueGrowth from 'assets/revenue-growth-image.png';
-import Grow from 'assets/grow-image.png';
-import EngineeringTeam from 'assets/engineering-team.png';
-import WordClassEngineers from 'assets/word-class-engineers.png';
+import breakpoint from 'styled-components-breakpoint';
+import { RevenueGrowth, Grow, X2, Crown } from 'components/icons';
 
-const StatisticsImage = styled.img`
-  width: 50%;
+const Wrapper = Flex.extend`
+  ${breakpoint('md')`
+  align-items: center;
+  `};
 `;
-
-const StatsImage = ({ src }) => (
-  <Col xs={6}>
-    <Margin bottom={15}>
-      <StatisticsImage src={src} />
-    </Margin>
-  </Col>
-);
 
 export default () => (
   <Section data-selector="stats">
@@ -28,16 +20,34 @@ export default () => (
       <Grid>
         <Row>
           <Col xs={12} md={6}>
-            <StatsImage src={RevenueGrowth} />
-            <H5 darker>Year on year revenue growth</H5>
-            <StatsImage src={Grow} />
-            <H5 darker>One of the fastest growing companies in the UK</H5>
-            <StatsImage src={EngineeringTeam} />
-            <H5 darker>
-              Our engineering team has doubled in size year on year
-            </H5>
-            <StatsImage src={WordClassEngineers} />
-            <H5 darker>World class engineers</H5>
+            <Wrapper column alignStart contentStretch justifyBetween full>
+              <Padding bottom={{ xs: 20 }}>
+                <FlexItem>
+                  <RevenueGrowth />
+                  <H5 darker>Year on year revenue growth</H5>
+                </FlexItem>
+              </Padding>
+              <Padding bottom={{ xs: 20 }}>
+                <FlexItem>
+                  <Grow />
+                  <H5 darker>One of the fastest growing companies in the UK</H5>
+                </FlexItem>
+              </Padding>
+              <Padding bottom={{ xs: 20 }}>
+                <FlexItem>
+                  <X2 />
+                  <H5 darker>
+                    Our engineering team has doubled in size year on year
+                  </H5>
+                </FlexItem>
+              </Padding>
+              <Padding bottom={{ xs: 20 }}>
+                <FlexItem>
+                  <Crown />
+                  <H5 darker>World class engineers</H5>
+                </FlexItem>
+              </Padding>
+            </Wrapper>
           </Col>
           <Col xs={12} md={6}>
             <Copy>
