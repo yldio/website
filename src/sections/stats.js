f1b7aa0f96f1ee41fd69e0cb4e460ecaffc39a11
Anna Lisowska <anna.lisowska@yld.io>
2018-07-11 17:41:57 +0100

    improvement mobile version for  stats icon container

--- src/sections/stats.js
+++ src/sections/stats.js
@@ -2,25 +2,37 @@ import React from 'react';
 import { Grid, Row, Col } from 'react-styled-flexboxgrid';
 import { Padding } from 'styled-components-spacing';
 import Flex, { FlexItem } from 'styled-flex-component';
+import styled from 'styled-components';
+import breakpoint from 'styled-components-breakpoint';
 
 import { Copy, H5 } from 'components/typography';
 import Section from 'components/section';
 import { RevenueGrowth, Grow, X2, Crown } from 'components/icons';
 
+const Wrapper = styled(Flex)`
+  align-items: center;
+  text-align: center;
+
+  ${breakpoint('md')`
+    align-items: flex-start;
+    text-align: initial;
+  `};
+`;
+
 export default () => (
   <Section data-selector="stats">
-    <Padding top={{ xs: 15, md: 36 }} bottom={{ xs: 15, md: 36 }}>
+    <Padding top={{ xs: 25, md: 36 }} bottom={{ xs: 15, md: 36 }}>
       <Grid>
         <Row>
           <Col xs={12} md={6}>
-            <Flex column alignStart contentStretch justifyBetween full>
-              <Padding bottom={{ xs: 15 }}>
+            <Wrapper column contentStretch justifyBetween full>
+              <Padding bottom={{ xs: 20 }}>
                 <FlexItem>
                   <RevenueGrowth />
                   <H5 darker>Year on year revenue growth</H5>
                 </FlexItem>
               </Padding>
-              <Padding bottom={{ xs: 15 }}>
+              <Padding bottom={{ xs: 20 }}>
                 <FlexItem>
                   <Grow />
                   <H5 darker>
@@ -28,7 +40,7 @@ export default () => (
                   </H5>
                 </FlexItem>
               </Padding>
-              <Padding bottom={{ xs: 15 }}>
+              <Padding bottom={{ xs: 20 }}>
                 <FlexItem>
                   <X2 />
                   <H5 darker>
@@ -36,13 +48,13 @@ export default () => (
                   </H5>
                 </FlexItem>
               </Padding>
-              <Padding bottom={{ xs: 15 }}>
+              <Padding bottom={{ xs: 20 }}>
                 <FlexItem>
                   <Crown />
                   <H5 darker>World class engineers</H5>
                 </FlexItem>
               </Padding>
-            </Flex>
+            </Wrapper>
           </Col>
           <Col xs={12} md={6}>
             <Padding top={{ md: 15 }}>
